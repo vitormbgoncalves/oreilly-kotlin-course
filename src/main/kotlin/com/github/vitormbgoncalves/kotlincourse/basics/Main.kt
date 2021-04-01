659f81a3091badbfe529f44a036bcbad0e8e88c8
vitormbgoncalves <vitor@innovativetecnologia.com.br>
2021-03-31 22:42:14 -0300

    Initial commit

---
+++ src/main/kotlin/com/github/vitormbgoncalves/kotlincourse/basics/Main.kt
@@ -0,0 +1,17 @@
+package com.github.vitormbgoncalves.kotlincourse.basics
+
+//renomear importacao de pacotes
+import com.github.vitormbgoncalves.kotlincourse.basics.utils.someUtility as someAditonalFunction
+
+/**
+ *  Packages and Imports in Kotlin & Summary of Kotlin
+ *
+ * @author Vitor Goncalves
+ * @since 31.03.2021, qua, 19:52
+ */
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    someAditonalFunction("Some name")
+}
---
+++ src/main/kotlin/com/github/vitormbgoncalves/kotlincourse/basics/utils/SampleUtils.kt
@@ -0,0 +1,5 @@
+package com.github.vitormbgoncalves.kotlincourse.basics.utils
+
+fun someUtility(name: String) {
+
+}
