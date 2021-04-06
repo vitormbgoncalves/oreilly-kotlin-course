86e10ca7ae5a82914cac715176fc43492feeb726
vitormbgoncalves <vitor@innovativetecnologia.com.br>
2021-04-06 17:17:18 -0300

    Working with nulls

---
+++ src/main/kotlin/com/github/vitormbgoncalves/kotlincourse/nulls/NullSafety.kt
@@ -0,0 +1,39 @@
+package com.github.vitormbgoncalves.kotlincourse.nulls
+
+/**
+ * Null Safety in Kotlin
+ *
+ * @author Vitor Goncalves
+ * @since 06.04.2021, ter, 16:30
+ */
+
+class Service {
+    fun evaluate() {}
+}
+class ServiceProvider {
+    fun createServices(): Service? = Service()
+}
+
+fun main(args: Array<String>) {
+    val message: String = "Message"
+
+    var nullMessage: String? = null
+    val inferredNull = null
+
+    // opcao 1
+    println(nullMessage?.length)
+
+    // opcao 2
+    if (nullMessage != null) {
+        println(nullMessage.length)
+    }
+
+    // opcao 3
+    println(nullMessage?.length)
+
+    val sp = createsServiceProvider()
+
+    sp?.createServices()?.evaluate()
+}
+
+private fun createsServiceProvider(): ServiceProvider? = ServiceProvider()
