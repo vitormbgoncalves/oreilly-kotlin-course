4c20c4b7ac9301df59b681b9751eb7ebfe55952f
vitormbgoncalves <vitor@innovativetecnologia.com.br>
2021-04-06 22:11:29 -0300

    A handful of tidbits

---
+++ src/main/kotlin/com/github/vitormbgoncalves/kotlincourse/tidbits/NotANumberException.kt
@@ -0,0 +1,42 @@
+package com.github.vitormbgoncalves.kotlincourse.tidbits
+
+import java.io.BufferedReader
+import java.io.FileReader
+
+/**
+ * Working with Exceptions in Kotlin
+ *
+ * @author Vitor Goncalves
+ * @since 06.04.2021, ter, 20:22
+ */
+
+class NotANumberException(message: String) : Throwable(message)
+
+fun checkIsNumber(obj: Any) {
+    when (obj) {
+        !is Int, Long, Float, Double -> throw NotANumberException("This is not a number")
+    }
+}
+
+fun main(args: Array<String>) {
+
+    /*try {
+        checkIsNumber("A")
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}")
+    } catch (e: NotANumberException) {
+        println("${e.message}")
+    }*/
+
+    val buffer = BufferedReader(FileReader("input.txt"))
+    val result = try {
+        val chars = CharArray(30)
+        buffer.read(chars, 0, 40)
+    } catch (e: IndexOutOfBoundsException) {
+        println("Exception handler")
+    } finally {
+        println("Clossing")
+        buffer.close()
+    }
+    println(result)
+}
---
+++ src/main/kotlin/com/github/vitormbgoncalves/kotlincourse/tidbits/Tuples.kt
@@ -0,0 +1,58 @@
+package com.github.vitormbgoncalves.kotlincourse.tidbits
+
+import com.github.vitormbgoncalves.kotlincourse.classes.CustomerKotlin
+
+/**
+ * Pair and Triple in Kotlin
+ *
+ * @author Vitor Goncalves
+ * @since 06.04.2021, ter, 18:05
+ */
+
+fun capitalAndPopulation(country: String): Pair<String, Long> {
+    return when (country) {
+        "Madrid" -> Pair("Madrid", 2300000)
+        "Brasilia" -> Pair("Brasilia", 2700000)
+        "Washington, D.C." -> Pair("Washington, D.C.", 692000)
+        else -> Pair("No capital", 0)
+    }
+}
+
+fun countyInformation(coutry: String): Triple<String, String, Long> {
+    return Triple("Madrid", "Europe", 2300000)
+}
+
+fun main(args: Array<String>) {
+
+    val result = capitalAndPopulation("Washington, D.C.")
+
+    println(result.first)
+    println(result.second)
+
+    val countryInfo = countyInformation("Madrid")
+
+    println()
+    println(countryInfo.first)
+    println(countryInfo.second)
+    println(countryInfo.third)
+
+    val (capital, continent, population) = countyInformation("Madrid")
+
+    println()
+    println(capital)
+    println(continent)
+    println(population)
+
+    val (id, name, email) = CustomerKotlin(1, "Vitor", "vitor@gmail.com")
+
+    println()
+    println(id)
+    println(name)
+    println(email)
+    println()
+
+    val listCapitalsAndCountry = listOf(Pair("Madrid", "Spain"), "Paris" to "France")
+
+    for ((capitals, country) in listCapitalsAndCountry)
+        println("$capitals - $country")
+}
