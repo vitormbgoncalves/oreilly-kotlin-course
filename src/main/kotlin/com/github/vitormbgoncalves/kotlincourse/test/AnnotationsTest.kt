1cea8f8c10d4ca6a81d12696330012cb2e53bf1c
vitormbgoncalves <vitor@innovativetecnologia.com.br>
2021-04-08 12:00:52 -0300

    Getting Functional

--- src/main/kotlin/com/github/vitormbgoncalves/kotlincourse/test/AnnotationsTest.kt
+++ src/main/kotlin/com/github/vitormbgoncalves/kotlincourse/test/AnnotationsTest.kt
@@ -1,17 +1,17 @@
-package com.github.vitormbgoncalves.kotlincourse.test
+/*package com.github.vitormbgoncalves.kotlincourse.test
 
 import org.junit.Test as Specification
 
-/**
+*//**
  * Annotations in Kotlin
  *
  * @author Vitor Goncalves
  * @since 06.04.2021, ter, 22:01
- */
+ *//*
 
 class AnnotationsTest {
 
     @Specification
     fun testAnnotation() {
     }
-}
+}*/
